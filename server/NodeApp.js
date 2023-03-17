881db8957312bd1a99b36e1488e28caf697c56fd
K2054852 <k2054852@kingston.ac.uk>
2023-03-17 21:16:16 +0000

    Changed middleware configuration to handle  up to 100 requests per minute

--- server/NodeApp.js
+++ server/NodeApp.js
@@ -1,14 +1,23 @@
-var request = require('request');
-var express = require('express');
+const express = require('express');
 const cors = require('cors');
-const router = require('./router');
+const router = require('./router.js');
 const path = require('path');
+const IP = "";
 const PORT= 5002;
-var app = express();
+const app = express();
 const bodyParser = require('body-parser');
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: "Too many requests, please try again later",
+});
+
+// apply the rate limiter to all requests
+app.use(limiter);
 
 app.use(cors());
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
