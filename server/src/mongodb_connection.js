d0c6ea2956bc06a67482c71a3a3bf5c870183443
K2054852 <k2054852@kingston.ac.uk>
2023-03-28 19:44:41 +0100

    Mongo db connection established

---
+++ server/src/mongodb_connection.js
@@ -0,0 +1,22 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const url = "mongodb+srv://alexdeveloper:zwWMyo0cEMANfT0C@cluster0.zy3cbgh.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(url, { useUnifiedTopology: true });
+
+const connect = async()=>{
+    try {
+        await client.connect();
+        console.log('Connected to MongoDB');
+      } catch (err) {
+        console.error(err);
+      }
+}
+
+const getConnection = ()=>{
+  return client.db(); 
+}
+
+module.exports = {
+    connect,
+    getConnection,
+};
