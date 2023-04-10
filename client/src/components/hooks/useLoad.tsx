2520c967968cda425ba0492fdd0fb0ee2de7fabc
K2054852 <k2054852@kingston.ac.uk>
2023-04-10 22:18:08 +0100

    Implemented react hook to load data from endpoint

---
+++ client/src/components/hooks/useLoad.tsx
@@ -0,0 +1,30 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { API } from "../../api/API";
+
+export const useLoad = (endpoint: string) =>{
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [status, setStatus] = useState('');
+    const load = (endpoint:string) =>{
+          setLoading(true)
+            axios.get(API+endpoint)
+              .then(response => {
+                if (response.data.result.length>0){
+                    console.log(response.data.result);
+                    setData(response.data.result);
+                    setStatus('Success')
+                } else {
+                    setLoading(false)
+                    setStatus('Could not find any data')
+                }
+              })
+              .catch(error => {
+                console.error(error);
+                setStatus('Error occured during loading')
+                setLoading(false)
+              });
+      }
+    useEffect(()=>{load()},[endpoint])
+    return {loading,status, load, data}
+}
